3e291292e6ba53b7f60ae4d66f47a5ed10a473b3
Piotr Kumala <piotrek.kumala@gmail.com>
2020-12-19 22:50:47 +0100

    add docker configuration for web api app

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -43,7 +43,7 @@ namespace WebApplication
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
