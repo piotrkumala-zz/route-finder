4be6eeda15b4f517b953ec30c16ef3bb6a20d817
Piotr Kumala <piotrek.kumala@gmail.com>
2020-12-20 03:07:32 +0100

    add create city method

---
+++ WebApplication/Controllers/CityController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Neo4jClient;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CityController : ControllerBase
+    {
+        private readonly ILogger<CityController> _logger;
+        private readonly IGraphClient _graphClient;
+
+        public CityController(ILogger<CityController> logger, IGraphClient graphClient)
+        {
+            _logger = logger;
+            _graphClient = graphClient;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<City>> Get()
+        {
+            var query = _graphClient.Cypher.Match("(city)").Return(city => city.As<City>());
+            return await query.ResultsAsync;
+        }
+
+        [HttpPost]
+        public async Task Add([FromBody] City city)
+        {
+            await _graphClient.Cypher.Create("(city:City $newCity)").WithParam("newCity", city)
+                    .ExecuteWithoutResultsAsync();
+        }
+    }
+}
---
+++ WebApplication/Models/City.cs
@@ -0,0 +1,9 @@
+namespace WebApplication.Models
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
